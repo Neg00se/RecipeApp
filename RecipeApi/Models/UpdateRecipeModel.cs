ae2cc11e7e9b31ec81c1ead73ffeececda5c5cfa
Neg00se <gooosb205@gmail.com>
2023-06-27 17:02:16 +0300

    Added Models For Update recipes and Create recipes

---
+++ RecipeApi/Models/UpdateRecipeModel.cs
@@ -0,0 +1,22 @@
+﻿using RecipeAppClassLibrary.Models;
+
+namespace RecipeApi.Models
+{
+	public class UpdateRecipeModel
+	{
+		
+
+		public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public TimeSpan CookingTime { get; set; }
+
+        public DifficultyModel Difficulty { get; set; }
+
+		public MealModel Meal { get; set; }
+
+        public CuisineModel Cuisine { get; set; }
+
+    }
+}
