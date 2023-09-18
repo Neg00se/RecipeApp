701ed135a62c5013d703c500af5e188ddb84b930
Neg00se <gooosb205@gmail.com>
2023-09-19 00:32:24 +0300

    added delete , update and rate mutations

--- recipeclient/src/features/recipes/recipeSlice.js
+++ recipeclient/src/features/recipes/recipeSlice.js
@@ -9,6 +9,7 @@ const initialState = recipeAdapter.getInitialState();
 
 export const recipeApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    //Recipe
     getRecipes: builder.query({
       query: () => "/Recipes/GetAllRecipes",
       transformResponse: (responseData) => {
@@ -19,6 +20,7 @@ export const recipeApiSlice = apiSlice.injectEndpoints({
         ...result.ids.map((id) => ({ type: "Recipe", id })),
       ],
     }),
+
     createRecipe: builder.mutation({
       query: ({ recipe, accessToken }) => ({
         url: "/Recipes/CreateRecipe",
@@ -29,7 +31,63 @@ export const recipeApiSlice = apiSlice.injectEndpoints({
 
       invalidatesTags: [{ type: "Recipe", id: "LIST" }],
     }),
+
+    updateRecipe: builder.mutation({
+      query: ({ recipe, accessToken }) => ({
+        url: "/Recipes/UpdateRecipe",
+        method: "PUT",
+        body: recipe,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Recipe", id: arg.id }],
+    }),
+
+    deleteRecipe: builder.mutation({
+      query: ({ recipeId, accessToken }) => ({
+        url: `/Recipes/delete/${recipeId}`,
+        method: "DELETE",
+        body: recipeId,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "Recipe", id: arg.recipeId },
+        { type: "Profile", id: "LIST" },
+      ],
+    }),
+
+    //Rate
+
+    rateRecipe: builder.mutation({
+      query: ({ rate, accessToken }) => ({
+        url: "/Rating/rateRecipe",
+        method: "POST",
+        body: rate,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "Recipe", id: arg.rate.recipeId },
+      ],
+    }),
+
+    deleteRecipeRate: builder.mutation({
+      query: ({ recipeId, accessToken }) => ({
+        url: `/Rating/deleteRate/${recipeId}`,
+        method: "DELETE",
+        body: recipeId,
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "Recipe", id: arg.recipeId },
+      ],
+    }),
   }),
 });
 
-export const { useGetRecipesQuery, useCreateRecipeMutation } = recipeApiSlice;
+export const {
+  useGetRecipesQuery,
+  useCreateRecipeMutation,
+  useRateRecipeMutation,
+  useDeleteRecipeRateMutation,
+  useDeleteRecipeMutation,
+  useUpdateRecipeMutation,
+} = recipeApiSlice;
