3f0257c34db1603444b717e0539bf2e7a151e434
Neg00se <gooosb205@gmail.com>
2023-09-19 00:22:41 +0300

    Add more prettier recipe display added time ago , rating value and now every recipe is a link to its own page

--- recipeclient/src/components/Recipe.js
+++ recipeclient/src/components/Recipe.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Card } from "react-bootstrap";
 import { useGetRecipesQuery } from "../features/recipes/recipeSlice";
+import TimeAgo from "./TimeAgo";
+import { Link } from "react-router-dom";
+import { Rating } from "@mui/material";
 
 const Recipe = ({ recipeId }) => {
   const { recipe } = useGetRecipesQuery("getRecipes", {
@@ -9,15 +12,37 @@ const Recipe = ({ recipeId }) => {
   });
 
   return (
-    <Row className=" border">
-      <Col className="sm-3 xs-2">
-        <Image src="https://picsum.photos/300" fluid />
-      </Col>
-      <Col sm={9}>
-        <div>{recipe.title}</div>
-        <div>{recipe.description}</div>
-      </Col>
-    </Row>
+    <Col md={6} lg={4}>
+      <Card className="me-1 h-100 recipeCard">
+        <Link to={`recipe/${recipe.id}`}>
+          <Card.Img src="https://picsum.photos/300/200" />
+        </Link>
+        <Card.Body as={Link} to={`recipe/${recipe.id}`} className="Link">
+          <Card.Title>
+            <h2>{recipe.title}</h2>
+          </Card.Title>
+          <span className="d-flex flex-row">
+            <Rating readOnly value={recipe.meanRating} />
+            <p>
+              &nbsp;
+              {recipe.ratingCount}
+              &nbsp;
+              {recipe.ratingCount === 1 ? "rate" : "rates"}
+            </p>
+          </span>
+
+          <Card.Text>{recipe.description.substring(0, 150)}...</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <div className="d-flex justify-content-end">
+            <span>
+              by {recipe.author.userName}{" "}
+              <TimeAgo timestamp={recipe.creationDateTime} />
+            </span>
+          </div>
+        </Card.Footer>
+      </Card>
+    </Col>
   );
 };
 
