4b6193d1436b763cd62763d740466ee11b455574
Neg00se <gooosb205@gmail.com>
2023-09-28 20:46:48 +0300

    Added Info Controller
    added controller for information about recipe meals and difficulties

---
+++ RecipeApi/Controllers/InfoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Models;
+using RecipeAppClassLibrary.Data;
+using RecipeAppClassLibrary.Models;
+
+namespace RecipeApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class InfoController : ControllerBase
+	{
+		private readonly RecipeAppDbContext _context;
+
+		public InfoController(RecipeAppDbContext context)
+		{
+			_context = context;
+		}
+
+
+
+		[HttpGet]
+		[Route("Meals")]
+
+		public async Task<List<MealModel>> GetMeals()
+		{
+			return await _context.Meals.ToListAsync();
+		}
+
+		[HttpGet]
+		[Route("Difficulties")]
+
+		public async Task<List<DifficultyModel>> GetDifficulties()
+		{
+			return await _context.Difficulty.ToListAsync();
+		}
+
+
+
+	}
+}
