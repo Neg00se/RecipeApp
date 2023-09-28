d714288942202a8350e3fa404be5e358e4a75203
Neg00se <gooosb205@gmail.com>
2023-09-28 20:50:46 +0300

    Add model for update create
    Added model for recipe creation and updation.

---
+++ RecipeApi/Models/CreateUpdateRecipeModel.cs
@@ -0,0 +1,28 @@
+﻿using RecipeAppClassLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApi.Models
+{
+	public class CreateUpdateRecipeModel
+	{
+        public int Id { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Title { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+		[Required]
+
+		public TimeSpan CookingTime { get; set; }
+
+		[Required]
+		public int DifficultyId { get; set; }
+		[Required]
+		public int MealId { get; set; }
+
+		public CuisineModel? Cuisine { get; set; }
+
+    }
+}
