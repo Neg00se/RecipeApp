d073502b151ff1dda79ca61af8fb9db4b48dd765
Neg00se <gooosb205@gmail.com>
2023-11-03 00:44:34 +0200

    Add Unit of Work class

---
+++ RecipeAppClassLibrary/DataAccess/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using RecipeAppData.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeAppData.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+	private readonly RecipeAppDbContext _context;
+
+	public UnitOfWork(RecipeAppDbContext context)
+    {
+		_context = context;
+	}
+
+	public async Task SaveAsync()
+	{
+		await _context.SaveChangesAsync();
+	}
+}
