670e76b8fc8c2501d38ec08749ff93edfa2ab50c
Neg00se <gooosb205@gmail.com>
2023-11-03 00:42:28 +0200

    Models moved to new project

---
+++ Shared/DtoModels/CreateUpdateRecipeModel.cs
@@ -0,0 +1,27 @@
+﻿using RecipeAppData.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DtoModels;
+
+public class CreateUpdateRecipeModel
+{
+	public int Id { get; set; }
+
+	[Required]
+	[MaxLength(50)]
+	public string Title { get; set; }
+
+	[Required]
+	public string Description { get; set; }
+	[Required]
+
+	public TimeSpan CookingTime { get; set; }
+
+	[Required]
+	public int DifficultyId { get; set; }
+	[Required]
+	public int MealId { get; set; }
+
+	public CuisineModel? Cuisine { get; set; }
+
+}
---
+++ Shared/DtoModels/UserProfileModel.cs
@@ -0,0 +1,19 @@
+﻿using RecipeAppData.Models;
+
+namespace Shared.DtoModels;
+
+//TODO: Remove this model coz it not necessary anymore
+public class UserProfileModel
+{
+	public string Id { get; set; }
+	public string UserName { get; set; }
+
+	public string Email { get; set; }
+
+	public int AuthoredRecipesCount { get; set; }
+
+	public List<RecipeModel> Recipes { get; set; }
+
+	public List<RatingModel> UserRates { get; set; }
+
+}
