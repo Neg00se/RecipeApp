f5d9faf64ab2327f0eea3978e255da6d8f26cb24
Neg00se <gooosb205@gmail.com>
2023-11-07 00:11:04 +0200

    Add Info Service
    Service for working with meals and difficulties catrgories logic

---
+++ Services/IInfoService.cs
@@ -0,0 +1,12 @@
+﻿using RecipeAppData.Models;
+
+namespace ServicesLayer
+{
+	public interface IInfoService
+	{
+		Task<List<DifficultyModel>> GetDifficultiesAsync();
+		Task<DifficultyModel> GetDifficultyAsync(int id);
+		Task<MealModel> GetMealAsync(int id);
+		Task<List<MealModel>> GetMealsAsync();
+	}
+}
---
+++ Services/InfoService.cs
@@ -0,0 +1,49 @@
+﻿using RecipeAppData.DataAccess;
+using RecipeAppData.DataAccess.Info;
+using RecipeAppData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLayer;
+
+public class InfoService : IInfoService
+{
+	private readonly IInfoRepository _infoRep;
+
+
+	public InfoService(IInfoRepository infoRep)
+	{
+		_infoRep = infoRep;
+
+	}
+
+	public async Task<List<MealModel>> GetMealsAsync()
+	{
+
+		var meals = await _infoRep.GetAllMeals();
+		return meals;
+	}
+
+	public async Task<List<DifficultyModel>> GetDifficultiesAsync()
+	{
+
+		var difficulties = await _infoRep.GetAllDifficulties();
+		return difficulties;
+	}
+
+	public async Task<MealModel> GetMealAsync(int id)
+	{
+		var meal = await _infoRep.GetMealById(id);
+		return meal;
+	}
+
+	public async Task<DifficultyModel> GetDifficultyAsync(int id)
+	{
+		var difficulty = await _infoRep.GetDifficultyById(id);
+		return difficulty;
+	}
+
+}
