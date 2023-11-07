0a17e897132b037c06649cf75b3e2aa2c2ccfeea
Neg00se <gooosb205@gmail.com>
2023-11-08 00:18:44 +0200

    Add Rating Service

---
+++ Services/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿using RecipeAppData.Models;
+
+namespace ServicesLayer
+{
+	public interface IRatingService
+	{
+		Task DeleteRate(int recipeId, UserModel user);
+		Task RateRecipe(int recipeId, int value, UserModel user);
+	}
+}
---
+++ Services/RatingService.cs
@@ -0,0 +1,55 @@
+﻿using RecipeAppData.DataAccess;
+using RecipeAppData.DataAccess.Rating;
+using RecipeAppData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLayer;
+
+public class RatingService : IRatingService
+{
+	private readonly IRatingRepository _ratingRepo;
+	private readonly IUnitOfWork _unitOfWork;
+
+	public RatingService(IRatingRepository ratingRepo, IUnitOfWork unitOfWork)
+	{
+		_ratingRepo = ratingRepo;
+		_unitOfWork = unitOfWork;
+	}
+
+	public async Task RateRecipe(int recipeId, int value, UserModel user)
+	{
+		bool isExist = _ratingRepo.IsRateExist(recipeId, user.Id);
+		if (isExist == false)
+		{
+			RatingModel rate = new RatingModel
+			{
+				RecipeModelId = recipeId,
+				Value = value,
+				UserModelId = user.Id
+
+			};
+			await _ratingRepo.AddRate(rate);
+		}
+		else
+		{
+			var rate = await _ratingRepo.GetRating(recipeId, user.Id);
+			rate.Value = value;
+			_ratingRepo.UpdateRate(rate);
+		}
+
+		await _unitOfWork.SaveAsync();
+	}
+
+
+	public async Task DeleteRate(int recipeId, UserModel user)
+	{
+		var rate = await _ratingRepo.GetRating(recipeId, user.Id);
+		_ratingRepo.DeleteRate(rate);
+		await _unitOfWork.SaveAsync();
+	}
+
+}
